179253018c54883fd91165f728f87177226880ba
veronica365 <naruth10@gmail.com>
2023-04-10 17:05:39 +0300

    Add router functionality and also modify the API fetch functionality

--- src/components/Missions/Missions.js
+++ src/components/Missions/Missions.js
@@ -1,6 +1,12 @@
-import { useSelector } from "react-redux";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMissions } from '../../redux/missions/missionslice';
 
 export default function Missions() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getMissions());
+  }, [dispatch]);
   const missions = useSelector((store) => store.missions.missions || []);
   return (
     <div>
@@ -10,7 +16,7 @@ export default function Missions() {
             <th>Mission</th>
             <th>Description</th>
             <th>Status</th>
-            <th></th>
+            <th>&nbsp;</th>
           </tr>
         </thead>
         <tbody>
@@ -18,8 +24,8 @@ export default function Missions() {
             <tr key={mission.mission_id}>
               <td>{mission.mission_name}</td>
               <td>{mission.mission_description}</td>
-              <td></td>
-              <td></td>
+              <td />
+              <td />
             </tr>
           ))}
         </tbody>
--- src/redux/missions/missionslice.js
+++ src/redux/missions/missionslice.js
@@ -1,8 +1,25 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { get } from "../api";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { get } from '../api';
+
+export const getMissions = createAsyncThunk(
+  'spacehub/getMissions',
+  async (_, thunkAPI) => {
+    try {
+      let data = await get('/missions');
+      data = (data || []).map((mission) => ({
+        mission_id: mission.mission_id,
+        mission_name: mission.mission_name,
+        mission_description: mission.description,
+      }));
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue('Something went wrong');
+    }
+  },
+);
 
 const missionSlice = createSlice({
-  name: "missions",
+  name: 'missions',
   initialState: {
     missions: [],
     isLoading: false,
@@ -14,25 +31,30 @@ const missionSlice = createSlice({
       missions: action.payload,
       isLoading: false,
     }),
+    setError: (state, action) => ({
+      ...state,
+      error: action.payload,
+      isLoading: false,
+    }),
+  },
+  extraReducers: {
+    [getMissions.pending]: (state) => ({
+      ...state,
+      isLoading: true,
+    }),
+    [getMissions.fulfilled]: (state, action) => ({
+      ...state,
+      missions: action.payload,
+      isLoading: false,
+    }),
+    [getMissions.rejected]: (state, action) => ({
+      ...state,
+      error: action.payload,
+      isLoading: false,
+    }),
   },
 });
 
 export const { setMissions } = missionSlice.actions;
-
-export const fetchMissions = () => async (dispatch) => {
-  try {
-    dispatch(setLoading(true));
-    let response = await get("missions");
-    response = response?.data?.map((data) => ({
-      id: data.id,
-      Missions_name: data.name,
-      description: data.description,
-      flickr_images: data.flickr_images,
-    }));
-    dispatch(setMissions(response));
-  } catch (error) {
-    dispatch(setError(error.message));
-  }
-};
 
 export default missionSlice.reducer;
