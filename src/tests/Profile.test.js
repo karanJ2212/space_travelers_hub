b82c550c457054d5495191d74df08c9d964b6840
veronica365 <naruth10@gmail.com>
2023-04-11 16:35:29 +0300

    Add testing for the profile and the navar components

---
+++ src/tests/Profile.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { Provider } from "react-redux";
+import MyProfile from "../components/MyProfile/MyProfile";
+import store from "../redux/store";
+
+it("matches snapshot", () => {
+  const tree = renderer
+    .create(
+      <Provider store={store}>
+        <MyProfile />
+      </Provider>
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
